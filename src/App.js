173952ef2944ca7f324380f2c1f70610acb36039
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-10-24 19:22:18 +0200

    Create Input Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+  state = {
+    inputText: ''
+  };
+
+  onTextInputChange = e => {
+    this.setState({
+      inputText: e.target.value
+    });
+  };
+
   render() {
-    return <div>Test</div>;
+    const { inputText } = this.state;
+    return <Input text={inputText} onChange={this.onTextInputChange} />;
   }
 }
+
+const Input = props => {
+  const { text, onChange, ...rest } = props;
+
+  return <input value={text} onChange={onChange} {...rest} />;
+};
+
+Input.propTypes = {
+  text: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
 
 export default App;
