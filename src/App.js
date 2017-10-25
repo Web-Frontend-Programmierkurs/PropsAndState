311a14d43aeb9f4bd460b792b1323a10d73d2568
Conrad Reuter <jacob.conrad.reuter@gmail.com>
2017-10-26 01:46:33 +0200

    Implement Todo List example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { v4 as uuid } from 'uuid'
+import CreateTodo from './CreateTodo'
+import TodoList from './TodoList'
+import './App.css'
 
 class App extends Component {
+
+  state = {
+    items: [],
+  }
+
   render() {
     return (
-      <div>Hello, world!</div>
+      <div className="app">
+        <h1>todo list</h1>
+        <CreateTodo create={this.create} />
+        <TodoList
+          delete={this.delete}
+          items={this.state.items}
+          toggle={this.toggle}
+        />
+      </div>
     )
+  }
+
+  create = text => {
+    const newItem = {
+      id: uuid(),
+      text,
+    }
+    const items = [newItem, ...this.state.items]
+    this.setState({ items })
+  }
+
+  delete = id => {
+    const items = [...this.state.items]
+    const index = items.findIndex(item => item.id === id)
+    if (index === -1) return
+    items.splice(index, 1)
+    this.setState({ items })
+  }
+
+  toggle = id => {
+    const items = [...this.state.items]
+    const index = items.findIndex(item => item.id === id)
+    if (index === -1) return
+    const toggledItem = { ...items[index], done: !items[index].done }
+    items.splice(index, 1, toggledItem)
+    this.setState({ items })
   }
 }
 
